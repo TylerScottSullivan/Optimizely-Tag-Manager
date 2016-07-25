42dc905ec910b3767dc6245f5a03e581b9420fc2
Mojiashen1 <mojiashen1@gmail.com>
2016-07-25 16:22:55 -0400

    add redirect

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET redirect page. */
+router.get('/redirect', function(req, res, next) {
+  res.render('redirect', { title: 'REDIRECT SUCCESS' });
+});
+
 module.exports = router;
