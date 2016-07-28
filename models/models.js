dd9cfc9292ee86f0bfb8b383342f3868242e3a7f
Mojiashen1 <mojiashen1@gmail.com>
2016-07-28 11:32:14 -0400

    change model

--- models/models.js
+++ models/models.js
@@ -4,10 +4,20 @@ var findOneOrCreate = require('mongoose-find-one-or-create')
 var projectSchema = mongoose.Schema({
   projectId: String,
   accountId: String,
-  tag: String,
-  trackingID: String,
-  trackingTrigger: String
+  tags: Array
+})
+
+var tagSchema = mongoose.Schema({
+  name: String,
+  trackingId: String,
+  trackingTrigger: String,
+  custom: String,
+  rank: Number,
+  projectId: String
 })
 
 projectSchema.plugin(findOneOrCreate);
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model({
+  'Project': projectSchema,
+  'Tag': tagSchema
+});
