b1c59cefa77eec25de456a37d8505b8ae352855b
Mojiashen1 <mojiashen1@gmail.com>
2016-08-01 14:46:59 -0400

    fb

--- snippets/google_universal.js
+++ snippets/google_universal.js
@@ -1,11 +1,11 @@
-module.exports = function(fields, loadedOnDocumentReady) {
+module.exports = function(fields, loadedOnDocumentReady, callback) {
   var TRACKING_ID = fields[0]['value']
   var ret = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ \
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\
 ga('create', '"+ TRACKING_ID + "', 'auto');\
-ga('send', 'pageview');"
+ga('send', 'pageview');ga("+ callback +")"
   if (loadedOnDocumentReady) {
     return '$(document).ready(function(){' +ret+ '})'
   } else {
