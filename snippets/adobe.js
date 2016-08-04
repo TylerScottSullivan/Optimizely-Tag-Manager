0ce1e6df6ecfb5460d9767987731a631bd73b09d
Caitlin <rubinca@sas.upenn.edu>
2016-08-04 16:25:32 -0400

    pushing

--- snippets/adobe.js
+++ snippets/adobe.js
@@ -1,6 +1,9 @@
-module.exports = function(fields, loadedOnDocumentReady) {
+module.exports = function(fields, loadedOnDocumentReady, callbacks) {
   var READ_TOKEN = fields[0]['value'];
   var WRITE_TOKEN = fields[1]['value'];
-  var ret = 'console.log('+ READ_TOKEN +'); console.log('+ WRITE_TOKEN +');'
+  var ret = 'console.log("'+ READ_TOKEN +'"); console.log('+ WRITE_TOKEN +');'
+  if (callbacks) {
+    ret += 'console.log("'+callbacks+'");'
+  }
   return ret;
 }
