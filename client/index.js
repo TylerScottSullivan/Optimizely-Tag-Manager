adbae99bcc42e5575b6b3e340b3425e128013b49
Tyler <tylerscott.sullivan@outlook.com>
2016-08-05 11:50:02 -0400

    Tyler's commits before merging with Mojia's

--- client/index.js
+++ client/index.js
@@ -1,9 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Modal = require('react-modal');
 var moment = require('moment');
+var reactace = require('react-ace');
+var Reactable = require('reactable');
+import { render } from 'react-dom';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import 'brace/mode/javascript';
+import 'brace/theme/github';
+import 'brace/theme/tomorrow';
+
 var _ = require('underscore');
-import { Button, Table } from 'optimizely-oui';
+import { Button } from 'optimizely-oui';
 import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
+
+
+var Table = Reactable.Table,
+    Thead = Reactable.Thead,
+    Th = Reactable.Th,
+    Tr = Reactable.Tr,
+    Td = Reactable.Td,
+    unsafe = Reactable.unsafe;
+
 
 var App = React.createClass({
   getInitialState: function() {
@@ -25,7 +44,6 @@ var App = React.createClass({
     return (
     	<div>
         <Tab/>
-        <SearchBar/>
         {/* add this */}
         {this.props.children}
       </div>
@@ -41,7 +59,7 @@ var Tab = React.createClass({
         <ul className="tabs-nav soft-double--sides">
           <Link to="/myTags" activeClassName="is-active" className="tabs-nav__item"><li className="tabs-nav__item" data-oui-tabs-nav-item>My Tags</li></Link>
           <Link to="/availableTags" activeClassName="is-active" className="tabs-nav__item"><li className="tabs-nav__item" data-oui-tabs-nav-item>Available Tags</li></Link>
-          <Link to="/createTag" activeClassName="is-active" className="tabs-nav__item"><li className="tabs-nav__item" data-oui-tabs-nav-item>Create Tag</li></Link>
+          <Link to="/submitNewTemplate" activeClassName="is-active" className="tabs-nav__item"><li className="tabs-nav__item" data-oui-tabs-nav-item>Submit New Template</li></Link>
         </ul>
         {/* add this */}
         {this.props.children}
@@ -50,38 +68,117 @@ var Tab = React.createClass({
   }
 })
 
-var SearchBar = React.createClass({
-  render: function() {
-    return (
-      <div className="flex--1 soft-double--sides">
-        <ul className="flex push-double--ends">
-          <li className="push-triple--right">
-            <div className="button-group">
-              <div> Need to put filter here </div>
-              <div className="search">
-                <input type="text" className="text-input text-input--search width--200" placeholder="Filter by Name"/>
-              </div>
-              <button className="button" type="button">Search</button>
-            </div>
-          </li>
-          <li className="anchor--right">
-            <button className="button button--highlight">Create Custom Tag</button>
-          </li>
-        </ul>
-      </div>
-    )
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    height                : '550px',
+    width                 : '700px'
   }
+};
+
+
+var SearchBar = React.createClass({
+	  getInitialState: function() {
+	  	console.log('got initial state')
+	    return { modalIsOpen: false };
+	  },
+	 
+	  openModal: function() {
+	  	console.log('opened modal')
+	    this.setState({modalIsOpen: true});
+	  },
+	 
+	  afterOpenModal: function() {
+	    // references are now sync'd and can be accessed. 
+	    this.refs.subtitle.style.color = '#0081BA';
+	  },
+	 
+	  closeModal: function() {
+	    this.setState({modalIsOpen: false});
+	  },
+	
+	render: function() {
+	    return (
+	    	<div>
+	        <ul className="flex push-double--ends">
+	          <li className="push-triple--right">
+	            <div className="button-group">
+	              <div> Need to put filter here </div>
+	              <div className="search">
+	                <input type="text" className="text-input text-input--search width--200" placeholder="Filter by Name"/>
+	              </div>
+	              <button className="button" type="button">Search</button>
+	            </div>
+	          </li>
+	          <li className="anchor--right">
+	            <button className="button button--highlight" onClick={this.openModal}>Create Custom Tag</button>
+				    <Modal
+				      isOpen={this.state.modalIsOpen}
+				      onAfterOpen={this.afterOpenModal}
+				      onRequestClose={this.closeModal}
+				      style={customStyles} >
+
+				      <h2 ref="subtitle">Create Custom Tag</h2>
+				      	<div className='modaltext'>
+					      <div> Please create your own tag by inserting HTML or Javascript </div>
+					      <div className="editor">
+					        <AceEditor
+					        	className="editor"
+							    mode="javascript"
+							    theme="tomorrow"
+							    name="UNIQUE_ID_OF_DIV"
+							    height="120px"
+							    width="620px"
+							    editorProps={{$blockScrolling: true}}
+							  />
+						  </div>
+				   		  <div className="flex">
+				               <div className="flex--1 sd-headsmall"> Description</div>
+				          </div>
+				          <div className="flex--1"> Please add the description of your tag below. </div>
+				          <textarea className='modaltextarea'/>
+						    <div className="flex">
+				               <div className="flex--1 sd-headsmall"> Called On: </div>
+				            </div>
+							    <select className="form-control" name='trackingTrigger'>
+							      <option value='inHeader'>In header</option>
+							      <option value='onPageLoad'>On page load</option>
+							    </select>
+				            <div className="flex">
+				               <div className="flex--1 sd-headsmall"> Enabled or Disabled: </div>
+				            </div>
+						    <select className="form-control" name='trackingTrigger'>
+						      <option value='inHeader'>Enabled</option>
+						      <option value='onPageLoad'>Disabled</option>
+						    </select>
+						  </div>
+						  <div className='flex space-between'>
+							  <button className="button button--highlight"> Add Custom Tag </button>
+						      <button className="button button--highlight" onClick={this.closeModal}> Close </button>
+					      </div>
+				    </Modal>
+	          </li>
+	        </ul>
+	      </div>
+	    )
+	  }
 })
 
 var MyTagsPage = React.createClass({
-  getInitialState() {
+  getInitialState: function() {
     return {
       splicedArray: [], //merge master templates and the downloaded project
     	sidePanel: {},
       currentProject: "6668600890"
     }
   },
-  componentDidMount() {
+  componentDidMount: function() {
     fetch('http://localhost:4001/master')
     .then((response) => response.json())
     .then(response =>{
@@ -135,14 +232,14 @@ var MyTagsPage = React.createClass({
 })
 
 var AvailableTagsPage = React.createClass({
-  getInitialState() {
+  getInitialState: function() {
     return {
       splicedArray: [],
     	sidePanel: {},
       currentProject: "6668600890"
     }
   },
-  componentDidMount() {
+  componentDidMount: function() {
     fetch('http://localhost:4001/master')
     .then((response) => response.json())
     .then(response => {
@@ -169,7 +266,7 @@ var AvailableTagsPage = React.createClass({
     console.log(this.state.sidePanel, " sidePanel in state");
   },
 
-  render() {
+  render: function() {
     return (
       <div className="flex height--1-1">
         <TableContent splicedArray={this.state.splicedArray} onSelect={this.onSelect}/>
@@ -180,12 +277,21 @@ var AvailableTagsPage = React.createClass({
 })
 
 var TableContent = React.createClass({
+ 
+  componentDidMount: function() {
+  	this.tableSort();
+  },
+
+  componentDidUpdate: function() {
+  	this.tableSort();
+  },
 
   render: function() {
     return (
-      <div>
+     	<div className="flex--1 soft-double--sides">
+     	<SearchBar/>
         <h1 className='header1'> My Tags </h1>
-        <table className="table table--rule table--hover">
+        <table className="table table--rule table--hover myTable" ref='myTable'>
           <thead>
             <tr>
               <th className = "cell-collapse">Logo</th>
@@ -198,29 +304,36 @@ var TableContent = React.createClass({
           <tbody>
             {
               this.props.splicedArray.map((rowinfo, item) => {
-                return <TableColumn onSelect={this.props.onSelect.bind(this, item, rowinfo)} key={item} name={rowinfo.name} called={rowinfo.trackingTrigger}/>
+                return <TableColumn onSelect={this.props.onSelect.bind(this, item, rowinfo)} key={item} rowinfo={rowinfo}/>
               })
             }
           </tbody>
         </table>
-      </div>
+        </div>
+
     )
+  },
+
+  tableSort: function() {
+  	  	$(this.refs.myTable).tablesorter();
   }
 })
 
 var TableColumn = React.createClass({
 	render: function() {
+		console.log(this.props, "props")
 		return (
    		 <tr onClick={this.props.onSelect}>
-          <td id="row-centered">GA LOGO</td>
-          <td id="row-centered">{this.props.name}</td>
-          <td id="row-centered"> Analytics </td>
-          <td id="row-centered">{this.props.called} </td>
+          <td id="row-centered"> <img src={this.props.rowinfo.logo}/></td>
+          <td id="row-centered">{this.props.rowinfo.displayName}</td>
+          <td id="row-centered">{this.props.rowinfo.category} </td>
+          <td id="row-centered">{this.props.rowinfo.called} </td>
           <td id="row-centered"> Enabled </td>
        </tr>
     )
 	}
 })
+
 
 
 var MySidePanel = React.createClass({
@@ -234,26 +347,40 @@ var MySidePanel = React.createClass({
 		if (Object.keys(this.props.info).length !== 0) {
 			return (
 				<div className="sidepanel background--faint">
-			      <h2 className="push-double--bottom">Experiment Details</h2>
-			      <div> {this.props.info.name} </div>
-            <div>{this.props.info.tagDescription}</div>
-            <label className="label label--rule">
-            </label>
-			        {this.props.info.fields.map(function(field, item) {
+			     	<h2 className="push-double--bottom sp-headbig">TAG DETAILS</h2>
+			      	<div className="flex">
+				    	<div> <img className='sidepanel-logo' src={this.props.info.logo}/> </div>
+				    	<div className='flex flex-v-center'>
+				      		<div className = 'sidepanel-displayname'> {this.props.info.displayName} </div>
+				     	</div>
+		        	</div>
+		        	<div className='sd-headsmall deschead'> DESCRIPTION </div>
+	            	<div className='tagdesc'>{this.props.info.tagDescription}</div>
+	            	<label className="label label--rule">
+	            	</label>
+			        {this.props.info.tokens.map(function(field, item) {
 			        	return <MyInputFields key={item} field={field}/>
 			        })}
-			      <div>Called on: </div>
-				    <select className="form-control" name='trackingTrigger'>
-				      <option value='inHeader'>In header</option>
-				      <option value='onPageLoad'>On page load</option>
-				    </select>
-			      <div> Enabled or Disabled? </div>
-				    <select className="form-control" name='trackingTrigger'>
-				      <option value='inHeader'>Enabled</option>
-				      <option value='onPageLoad'>Disabled</option>
-				    </select>
-				  <div><button className="button button--highlight">Update</button>
-				  <button className="button button--highlight">Delete</button></div>
+		            <div className="flex">
+		               <div className="flex--1 sd-headsmall"> Called On: </div>
+		            </div>
+					    <select className="form-control" name='trackingTrigger'>
+					      <option value='inHeader'>In header</option>
+					      <option value='onPageLoad'>On page load</option>
+					    </select>
+		            <div className="flex">
+		               <div className="flex--1 sd-headsmall"> Enabled or Disabled: </div>
+		            </div>
+					    <select className="form-control" name='trackingTrigger'>
+					      <option value='inHeader'>Enabled</option>
+					      <option value='onPageLoad'>Disabled</option>
+					    </select>
+				    <div> 
+				    	<button className="btn-uniform-add button button--highlight">Update Tag</button>
+					</div>
+					<div>
+						<button className="btn-uniform-del button button--highlight">Delete</button>
+			  		</div>
 			  </div>
 			)
 		} else {
@@ -263,107 +390,94 @@ var MySidePanel = React.createClass({
 })
 
 var AvailableSidePanel = React.createClass({
-  getInitialState: function() {
-    return {
-      info: null,
-//field
-//
-    };
-  },
-
-  componentWillReceiveProps: function(nextProps) {
-    if (nextProps.info) {
-      this.setState({
-        info: nextProps.info
-      })
-    }
-  },
-
-  onAddTag: function() {
-
-    return $.ajax({
-      url: '/',
-      type: 'POST',
-      data: {
-        'ACCESS_TOKEN': this.state.ACCESS_TOKEN,
-        'ACCESS_TOKEN': this.state.ACCESS_TOKEN,
-      },
-      success: function(data) {
-        console.log('Add tag successful')},
-      error: function(err) {
-        console.error("Err posting", err.toString());
-      }.bind(this)
-    });
-  },
-
-  onChange: function(field, e) {
-    var newState = Object.assign({}, this.state);
-    newState[field] = e.target.value;
-    this.setState(newState);
+  onDelete: function() {
+    //delete a tag
+    // POST 'deletetag/:tagid'
+    return null
   },
 
 	render: function() {
 		if (Object.keys(this.props.info).length !== 0) {
 			return (
 				<div className="sidepanel background--faint">
-			      <h2 className="push-double--bottom">Experiment Details</h2>
-			      <div> {this.props.info.name} </div>
-            <div>{this.props.info.tagDescription}</div>
-            <label className="label label--rule">
-            </label>
+			     	<h2 className="push-double--bottom sp-headbig">TAG DETAILS</h2>
+			      	<div className="flex">
+				    	<div> <img className='sidepanel-logo' src={this.props.info.logo}/> </div>
+				    	<div className='flex flex-v-center'>
+				      		<div className = 'sidepanel-displayname'> {this.props.info.displayName} </div>
+				     	</div>
+		        	</div>
+		        	<div className='sd-headsmall deschead'> DESCRIPTION </div>
+	            	<div className='tagdesc'>{this.props.info.tagDescription}</div>
+	            	<label className="label label--rule">
+	            	</label>
 			        {this.props.info.tokens.map(function(field, item) {
-			        	return <AvailableInputFields key={item} field={field} value={field.value} onChange={this.onChange.bind(this, field)}/>
+			        	return <MyInputFields key={item} field={field}/>
 			        })}
-			      <div>Called on: </div>
-				    <select className="form-control" name='trackingTrigger'>
-				      <option value='inHeader'>In header</option>
-				      <option value='onPageLoad'>On page load</option>
-				    </select>
-			      <div> Enabled or Disabled? </div>
-				    <select className="form-control" name='trackingTrigger'>
-				      <option value='inHeader'>Enabled</option>
-				      <option value='onPageLoad'>Disabled</option>
-				    </select>
-				  <button className="button button--highlight" onClick={this.onAddTag}>Add</button>
+		            <div className="flex">
+		               <div className="flex--1 sd-headsmall"> Called On: </div>
+		            </div>
+					    <select className="form-control" name='trackingTrigger'>
+					      <option value='inHeader'>In header</option>
+					      <option value='onPageLoad'>On page load</option>
+					    </select>
+		            <div className="flex">
+		               <div className="flex--1 sd-headsmall"> Enabled or Disabled: </div>
+		            </div>
+					    <select className="form-control" name='trackingTrigger'>
+					      <option value='inHeader'>Enabled</option>
+					      <option value='onPageLoad'>Disabled</option>
+					    </select>
+				    <div> 
+				    	<button className="btn-uniform-add button button--highlight">Add Tag</button>
+					</div>
 			  </div>
 			)
 		} else {
-			return <div> </div>;
+			return <div> 
+
+				<div className="sidepanel background--faint">
+			     	<h2 className="push-double--bottom sp-headbig">TAG DETAILS</h2>
+					<div> Select a Tag to add to My Tags. </div>
+			  	</div>
+
+
+
+			</div>;
 		}
 	}
-});
+})
 
 var MyInputFields = React.createClass({
 	render: function () {
+		console.log(this.props, "props")
 		return (
-				<div>
-				    <label className="label label--rule">
-		            <div className="flex">
-		               <div className="flex--1">{this.props.field.name}</div>
-		            </div>
-			        <div> {this.props.field.description} </div>
-			        <input placeholder={this.props.field.value} />
-              </label>
+			<div>
+	            <div className="flex">
+	               <div className="flex--1 sd-headsmall">{this.props.field.tokenName}</div>
+	            </div>
+		        <div> {this.props.field.description} <a href={this.props.field.learnmorelink} target="_blank"> Learn More. </a> </div>
+		        <input className='text-input width--200 text-input-styled' placeholder={this.props.field.placeholder} />
 		    </div>
 		)
 	}
 })
 
-var AvailableInputFields = React.createClass({
-	render: function () {
-		return (
-				<div>
-				    <label className="label label--rule">
-		            <div className="flex">
-		               <div className="flex--1">{this.props.field.tokenName}</div>
-		            </div>
-			        <div> {this.props.field.description} </div>
-			        <input value={this.props.value} onChange={this.props.onChange}/>
-              </label>
-		    </div>
-		)
-	}
-})
+// var AvailableInputFields = React.createClass({
+// 	render: function () {
+// 		return (
+// 				<div>
+// 				    <label className="label label--rule">
+// 		            <div className="flex">
+// 		               <div className="flex--1">{this.props.field.tokenName}</div>
+// 		            </div>
+// 			        <div> {this.props.field.description} </div>
+// 			        <input classname='text-input' value={this.props.value} onChange={this.props.onChange}/>
+//               </label>
+// 		    </div>
+// 		)
+// 	}
+// })
 
 var SidePanelAdding = React.createClass({
 	render: function() {
@@ -379,7 +493,7 @@ ReactDOM.render((
       {/* make them children of `App` */}
       <Route path="/myTags" component={MyTagsPage}/>
       <Route path="/availableTags" component={AvailableTagsPage}/>
-      <Route path="/createTag" component={AvailableTagsPage}/>
+      <Route path="/submitNewTemplate" component={AvailableTagsPage}/>
     </Route>
   </Router>
 ), document.getElementById('root'))
