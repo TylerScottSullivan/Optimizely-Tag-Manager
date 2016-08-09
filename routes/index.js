c56c3268ae61dd6cf12c35897c53cedf63d6c5a2
Caitlin <rubinca@sas.upenn.edu>
2016-08-09 18:37:10 -0400

    delete works now

--- routes/index.js
+++ routes/index.js
@@ -88,12 +88,15 @@ router.post('/', function(req, res, next) {
 });
 
 router.post('/deletetag/:tagid', function(req, res, next) {
+  var utils = require('../utils')
   Tag.remove({"_id": req.params.tagid})
-     .then(utils.getProject.bind(utils))
+     .then(utils.getProject.bind(utils, 6668600890, req.params.tagid))
+     .then(utils.removeTagFromProject.bind(utils))
      .then(utils.populateProject.bind(utils))
      .then(utils.getJavascript.bind(utils))
      .then(utils.buildJavascript.bind(utils))
      .then(function(response) {
+       console.log("GOT TO THE END OF THE DELTE CHAIN")
        res.status(200).send('I am alright')
      })
      .catch(function(err) {
@@ -104,7 +107,6 @@ router.post('/deletetag/:tagid', function(req, res, next) {
 // /masters
 // GET: gets all current master templates
 router.get('/master', (req, res, next) => {
-  var utils = require('../utils')
   Master.find(function(err, masters) {
     if (err) {
       console.log("err found in finding masters", err)
