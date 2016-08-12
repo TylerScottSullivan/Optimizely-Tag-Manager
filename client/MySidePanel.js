bcbf8d1bf96acda4ab122aeccc08484f9bba21ad
Tyler <tylerscott.sullivan@outlook.com>
2016-08-12 04:04:57 -0400

    fixed enabled disabled button again

--- client/MySidePanel.js
+++ client/MySidePanel.js
@@ -176,7 +176,10 @@ var MySidePanel = React.createClass({
   //       <button className="btn-uniform-del button button--highlight" onClick={this.onDelete}>Delete</button>
   //     </div>)
   // }
-
+console.log(this.state.triggerOptions, "trigger options");
+console.log(this.state.trackingTrigger, "tracking trigger current")
+console.log(this.state.info.trackingTrigger, "tracking trigger info")
+console.log(this.props.info.trackingTrigger, "tracking trigger props")
     if(this.props.deleted) {
       console.log('this should delete it')
       return (
@@ -223,131 +226,99 @@ var MySidePanel = React.createClass({
                       }
                     }
 
-                  var okay = null;
-                  if (this.state.active) {
-                    okay =  <div>
-                                  <button className="button button--highlight" name='active' onClick={this.onChange}>Enabled</button>
-                                  <button className="button" name='active' onClick={this.onChange}>Disabled</button>
-                                </div>
-
-                  } else {
-                    okay =  <div>
-                                  <button className="button" name='active' onClick={this.onChange}>Enabled</button>
-                                  <button className="button button--highlight" name='active' onClick={this.onChange}>Disabled</button>
-                                </div>
-                  }
-
-                  var pic = null;
-                  if (this.state.info.logo) {
-                    pic =  <div> <img className='sidepanel-logo' src={this.state.info.logo}/> </div>
-                  } else {
-                    pic =  <div> <img className='sidepanel-logo' src="images/custom.png"/> </div>
-                  }
 
 
                   // console.log(this.props, "props for mySidePanel")
               		if ((this.props.info && Object.keys(this.props.info).length !== 0) || this.props.deleted) {
                         console.log(this.props.deleted, "deleted state and NOT EMPTY TAG DETAILS");
               			return (
-              				<div data-toggle='validator' className="sidepanel background--faint">
-              			     	<h2 className="push-double--bottom sp-headbig">TAG DETAILS</h2>
-                          {!this.props.deleted ? 
-                            <div>
-              			      	<div className="flex">
-                                {pic}
-              				    	<div className='flex flex-v-center'>
-              				      		<div className = 'sidepanel-displayname'> {this.state.info.displayName} </div>
-              				     	</div>
-              		        	</div>
-              		        	<div className='sd-headsmall deschead'> DESCRIPTION </div>
-              	            	<div className='tagdesc'>{this.state.info.tagDescription}</div>
-              	            	<label className="label label--rule">
-              	            	</label>
-                            </div>
-                              :
-                              <div> </div>
-                            }
-                              {this.props.info.name === "custom" && !this.props.deleted ?
-                                <div>
-                                  <button className="btn-uniform-add button button--highlight" onClick={this.openModal}> Edit Custom Code</button>
-
-                                  <Modal
-                                    isOpen={this.state.modalIsOpen}
-                                    onAfterOpen={this.afterOpenModal}
-                                    onRequestClose={this.closeModal}
-                                    style={customStyles} >
-
-                                    <h2 ref="subtitle">Update Custom Tag</h2>
-                                    <div className='modaltext'>
-                                      <div> Please update your Javascript code here. </div>
-                                      <div className="editor">
-                                        <AceEditor
-                                          className="editablecustom"
-                                          mode="javascript"
-                                          theme="tomorrow"
-                                          name="editablecustom"
-                                          height="120px"
-                                          width="620px"
-                                          editorProps={{$blockScrolling: true}}
-                                          value={this.props.info.template}
-                                          onChange={this.onChangeSnippet}
-                                        />
-                                      </div>
-                                    </div>
-                                    <div className='flex space-between'>
-                                      <button className="button button--highlight"> Update Custom Tag-Not Functional </button>
-                                      <button className="button button--highlight" onClick={this.closeModal}> Cancel </button>
-                                    </div>
-                                  </Modal>
+                          <div data-toggle='validator' className="sidepanel background--faint">
+                              <h2 className="push-double--bottom sp-headbig">TAG DETAILS</h2>
+                                <div className="flex">
+                                {this.state.info.logo ? 
+                                    <div> <img className='sidepanel-logo' src={this.state.info.logo}/> </div>
+                                  :
+                                    <div> <img className='sidepanel-logo' src="images/custom.png"/> </div>
+                                }
+                                <div className='flex flex-v-center'>
+                                    <div className = 'sidepanel-displayname'> {this.state.info.displayName} </div>
                                 </div>
-                                :
-                                <div> </div>
-                              }
-                                <div>
-              			               {splicedTokenField.map(function(field, item) {
+                                </div>
+                                <div className='sd-headsmall deschead'> DESCRIPTION </div>
+                                  <div className='tagdesc'>{this.state.info.tagDescription}</div>
+                                  <label className="label label--rule">
+                                  </label>
+                                  {this.props.info.name === "custom" ?
+                                    <div>
+                                      <button className="btn-uniform-add button button--highlight" onClick={this.openModal}> Edit Custom Code</button>
+
+                                      <Modal
+                                        isOpen={this.state.modalIsOpen}
+                                        onAfterOpen={this.afterOpenModal}
+                                        onRequestClose={this.closeModal}
+                                        style={customStyles} >
+
+                                        <h2 ref="subtitle">Update Custom Tag</h2>
+                                        <div className='modaltext'>
+                                          <div> Please update your Javascript code here. </div>
+                                          <div className="editor">
+                                            <AceEditor
+                                              className="editablecustom"
+                                              mode="javascript"
+                                              theme="tomorrow"
+                                              name="editablecustom"
+                                              height="120px"
+                                              width="620px"
+                                              editorProps={{$blockScrolling: true}}
+                                              value={this.props.info.template}
+                                              onChange={this.onChangeSnippet}
+                                            />
+                                          </div>
+                                        </div>
+                                        <div className='flex space-between'>
+                                          <button className="button button--highlight"> Update Custom Tag-Not Functional </button>
+                                          <button className="button button--highlight" onClick={this.closeModal}> Cancel </button>
+                                        </div>
+                                      </Modal>
+                                    </div>
+                                    :
+                                    <div> </div>
+                                  }
+                                {splicedTokenField.map(function(field, item) {
                                   var err = this.state.errors[field.name];
-              			        	    return <MyInputFields key={item} error={err || false} field={field} value={this.state.fields[item].value} onChange={this.onChangeTokens.bind(this, item)}/>
-              			               }.bind(this))}
-                  		            <div className="flex">
-                  		               <div className="flex--1 sd-headsmall"> Called On: </div>
-                  		            </div>
-                    				       <select className="form-control" name='trackingTrigger' onChange={this.onChange}>
-                                      {this.state.triggerOptions.map((trigger) => {
-                                        if (trigger === this.state.info.trackingTrigger) {
-                                          return <option value={trigger} selected> {trigger} </option>
-                                        }
-                                        return <option value={trigger} >{trigger}</option>
-                                        })
-                                      }
-                                  </select>
-                            </div>
-                          <div className="flex togglebutton">
-                            {!this.props.deleted ?       
-                                 okay
-                             :
-                              <div> </div>
-                              }
-                          </div>
-                          {!this.props.deleted ?
-                          <div> 
-              				    <div>
-              				    	<button className="btn-uniform-add button button--highlight" onClick={this.onUpdate}>Update Tag</button>
-                          </div>
-                          <div>
-              						  <button className="btn-uniform-del button button--highlight" onClick={this.onDelete}>Delete</button>
-                          </div>
-                          </div>
-                          :
-                          <div>
-                            <div className="redbox">
-                              <p> This tag has now been deleted. This change may take up to 10 minutes before it is updated within your Optimizely tag. </p>
-                              <p> To Re-Add this tag, go to your "Available Tags" tab. </p>
-                            </div>
-                            <div>
-                              <button className="btn-uniform-del button button--highlight" onClick={this.onDelete}>Delete</button>
-                            </div>
-                          </div>
-                        }
+                                  return <MyInputFields key={item} error={err || false} field={field} value={this.state.fields[item].value} onChange={this.onChangeTokens.bind(this, item)}/>
+                                }.bind(this))}
+                                  <div className="flex">
+                                     <div className="flex--1 sd-headsmall"> Called On: </div>
+                                  </div>
+                            <select className="form-control" name='trackingTrigger' onChange={this.onChange}>
+                                  {this.state.triggerOptions.map((trigger) => {
+                                    if (trigger === this.state.info.trackingTrigger) {
+                                      return <option value={trigger} selected> {trigger} </option>
+                                    }
+                                    return <option value={trigger} >{trigger}</option>
+                                    })
+                                  }
+                              </select>
+                              <div className="flex togglebutton">
+                                {this.state.info.active === true ?
+                                    <div>
+                                      <button className="button button--highlight" name='active' onClick={this.onChange}>Enabled</button>
+                                      <button className="button" name='active' onClick={this.onChange}>Disabled</button>
+                                    </div>
+                                 :
+                                    <div>
+                                      <button className="button" name='active' onClick={this.onChange}>Enabled</button>
+                                      <button className="button button--highlight" name='active' onClick={this.onChange}>Disabled</button>
+                                    </div>
+                                  }
+                              </div>
+                              <div>
+                                <button className="btn-uniform-add button button--highlight" onClick={this.onUpdate}>Update Tag</button>
+                              </div>
+                              <div>
+                                <button className="btn-uniform-del button button--highlight" onClick={this.onDelete}>Delete</button>
+                              </div>
                           {this.state.clickUpdate ? 
                             <div className="yellowbox">
                               This tag has now been updated. This change may take up to 10 minutes before it is updated within your Optimizely tag.
