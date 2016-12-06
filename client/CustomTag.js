b26e9ba688f4abe36f591a346c8c1c2ae703b75a
Tyler <tylerscott.sullivan@outlook.com>
2016-12-06 12:17:26 -0800

    set options state correclty, fixed ATP loading bug

--- client/CustomTag.js
+++ client/CustomTag.js
@@ -125,6 +125,7 @@ var CustomTag = React.createClass({
   				         	onChange={this.changeSnippet}
   				        />
   				    </div>
+  				    <div> Hello </div>
 		            <DisplayName onChange={this.changeDisplayName} displayName={this.state.displayName}/>
 		            <CustomDescription onChange={this.changeCustomDescription} tagDescription={this.state.tagDescription}/>
 		            <ToggleButton onChange={this.changeToggleButton} active={this.state.active}/>
